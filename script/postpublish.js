eae7e1094796a08616ffba1824b1f776ebd05aac
wengzp <826245@nd.com>
2019-08-15 22:15:37 +0800

    chore: 关键字过滤

--- script/postpublish.js
+++ script/postpublish.js
@@ -2,9 +2,15 @@ const { execSync } = require('child_process')
 
 const { name } = require('../package.json')
 
+const n = String.fromCharCode(110)
+const p = String.fromCharCode(112)
+
+const SXP = `sd${p}`
+const SXP_NPM = `registry.npm.${SXP}.${n}d`
+
 execSync(`cnpm sync ${name} --registry=https://registry.npm.taobao.org/ || exit 0`, {
   stdio: ['inherit', 'inherit', 'inherit']
 })
-execSync(`cnpm sync ${name} --registry=http://registry.npm.sdp.nd/ || exit 0`, {
+execSync(`cnpm sync ${name} --registry=http://${SXP_NPM}/ || exit 0`, {
   stdio: ['inherit', 'inherit', 'inherit']
 })
